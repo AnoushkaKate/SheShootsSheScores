17e6dd959d41fd6ad0c7abb765a8dab6a63e2435
AnoushkaPaul <paul.a2@maristcollege.school.nz>
2023-10-21 01:38:45 +1300

    about carousel - images and league pages basic CSS

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ function myFunction() {
   
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
